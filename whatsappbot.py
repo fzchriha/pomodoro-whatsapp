a4c99ba098bea866cc16e6a35fa71f5fe61df9ee
Fatima Zahra Chriha <fatimazahra.chriha@gmail.com>
2020-07-15 13:25:08 -0400

    Pomodoro clock still need to adjust the time sleep

--- whatsappbot.py
+++ whatsappbot.py
@@ -1,16 +1,41 @@
-from selenium import webdriver
-import time 
-chrome_browser = webdriver.Chrome(executable_path='/Users/fatimazahra.chriha/Desktop/Recurse/pomodoro-whatsapp/chromedriver')
-chrome_browser.get('https://web.whatsapp.com/')
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import schedule
+import time
+import emoji 
+app = Flask(__name__)
 
-time.sleep(15)
+@app.route("/")
+def hello():
+    return "Hi I am your Pomodoro Bot! Reply 'start' to begin your productivity session"
 
-print('Scanning is completed...')
-username_list = ['Pomodoro Bot', 'Whatsapp Bot', 'Mariam Khti']
-for username in username_list:
-    user = chrome_browser.find_element_by_xpath('//span[@title="{}"]'.format(username))
-    user.click()
-    message_box = chrome_browser.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
-    message_box.send_keys('Hey, I am your pomodoro bot')
-    send_button = chrome_browser.find_element_by_xpath('//button[@class="_1U1xa"]')
-    send_button.click()
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+
+    # Fetch the message
+    msg = request.form.get('Body')
+    resp = MessagingResponse()
+    # # Create reply
+    
+    if msg == 'Hello':
+        resp.message("Hi I am your Pomodoro Bot!\n Reply *Start* to begin your productivity session \N{hourglass}")
+        return str(resp)
+    elif msg == 'Start':
+        print("hey")
+        resp.message("You did a great job! Reply *Break* to take a break")
+        time.sleep(6)
+        return str(resp)
+    elif msg == "Break":
+        print("hey")
+        resp.message("Hmm feels good to have a break \N{hot beverage} !Reply *Start* to begin your productivity session")
+        time.sleep(6)
+        return str(resp)
+
+    resp.message("Hello, I am your Pomodoro Bot \N{tomato}")
+    print(emoji.demojize('⌛️'))
+
+    return str(resp)
+
+if __name__ == "__main__":
+    app.run(debug=True)
