67bf17a376c5c4b743a8b2bee9d478b4e771f271
Fatima Zahra Chriha <fatimazahra.chriha@gmail.com>
2020-07-18 13:43:39 -0400

    Testing

--- whatsappbot.py
+++ whatsappbot.py
@@ -14,7 +14,7 @@ client = Client(account, token)
 
 # message = client.messages.create(to=user_number(), from_="whatsapp:+14155238886",
 #
-@app.route("/", methods=['POST'])                                  body="Hello there!")
+@app.route("/", methods=['GET'])                                  body="Hello there!")
 def Hello():
     return "Hello Team RC"
 @app.route("/sms", methods=['POST'])
