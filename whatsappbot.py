759cb470215cd78fcc007951bfd9ee84ca64dba6
Fatima Zahra Chriha <fatimazahra.chriha@gmail.com>
2020-07-21 13:04:28 -0400

    Improving testing

--- whatsappbot.py
+++ whatsappbot.py
@@ -49,8 +49,8 @@ def sms_reply():
     if msg == 'Done':
     	client.messages.create(to=phone_number, from_="whatsapp:+14155238886", body='Not yet!')
     if msg == 'Debug':
-    	resp.message(tmp)
-    	return str(resp)
+    	time_left = tmp + datetime.timedelta(minutes=25) - datetime.datetime.now()
+    	client.messages.create(to=phone_number, from_="whatsapp:+14155238886", body=time_left)
     elif msg == "Break":
     	sched = BackgroundScheduler(daemon=True)
     	sched.add_job(pomodoro, 'date', run_date=brek_session, args=[brek_msg])
